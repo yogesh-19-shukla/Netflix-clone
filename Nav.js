7fd16203e1d7836d789a445f4d094de99d44a413
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2021-07-07 02:15:17 -0700

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,29 @@
+import React ,{useState,useEffect} from 'react';
+import './Nav.css';
+
+
+function Nav() {
+
+    const [show,handleShow] = useState(false); 
+
+    useEffect(() => {
+      window.addEventListener("scroll",()=>{
+          if(window.scrollY > 100){
+              handleShow(true);
+          }else handleShow(false);
+      });
+      return () => {
+          window.removeEventListener("scroll",null);
+      };
+    },[]);
+
+    return (
+        <div className={`nav ${show && "nav_black"}`}>
+        <img className="nav_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/640px-Netflix_2015_logo.svg.png" alt="Netflix Logo" />
+        <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/640px-Netflix-avatar.png" alt="Netflix Logo" />
+
+        </div>
+    )
+}
+
+export default Nav
